0fd2c2ce54f3b213bbd13ac5c6e41cd92711f6ac
Matyrobbrt <matyrobbrt@gmail.com>
2022-05-14 19:40:08 +0300

    Setup base mod

---
+++ src/main/java/com/matyrobbrt/sectionprotection/SectionProtection.java
@@ -0,0 +1,17 @@
+package com.matyrobbrt.sectionprotection;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SectionProtection.MOD_ID)
+public class SectionProtection {
+
+	public static final String MOD_ID = "sectionprotection";
+	public static final Logger LOGGER = LogUtils.getLogger();
+
+	public SectionProtection() {
+	}
+}
